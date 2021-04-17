9a2d124f1c7c3de90db184ea1f6e4e771012008b
Yoni Messing <Jonathan.lev.messing@gmail.com>
2021-04-17 01:39:01 -0400

    added tests and working solution

---
+++ app.js
@@ -0,0 +1,68 @@
+const readline = require("readline");
+const fs = require("fs");
+
+const readInterface = readline.createInterface({
+  input: fs.createReadStream("./input.txt"),
+  output: process.stdout,
+  terminal: false,
+});
+
+const state = {};
+readInterface.on("line", function (line) {
+  line = line.split(" ");
+  let driver = line[0];
+  let name = line[1];
+
+  if (driver === "Driver" && !state[name]) {
+    state[name] = new Trip();
+  }
+
+  if (driver === "Trip") {
+    let [, name, start, stop, miles] = line;
+    state[name]["mins"] = state[name]["mins"] += timeDriving(start, stop);
+    state[name]["miles"] = state[name]["miles"] += parseFloat(miles);
+    state[name]["mph"] = speed(state[name]["miles"], state[name]["mins"]);
+  }
+});
+
+readInterface.on("close", function () {
+  fs.writeFile("output.txt", writeToFile(state), function (err) {
+    if (err) {
+      return console.log(err);
+    }
+  });
+});
+
+function speed(distance, minutes) {
+  return Math.round(parseFloat(distance) / (parseFloat(minutes) / 60.0));
+}
+
+function writeToFile(state) {
+  let data = "";
+  for (prop in state) {
+    let mph = state[prop]["mph"];
+    let miles = state[prop]["miles"];
+    data += `${prop}: ${miles} miles @ ${mph} mph \n`;
+  }
+  return data;
+}
+
+function timeDriving(start, stop) {
+  let startTime = new Date(`Fri Apr 16 2021 ${start}`);
+  let endTime = new Date(`Fri Apr 16 2021 ${stop}`);
+  let res = Math.abs(startTime - endTime) / 1000;
+  let mins = Math.floor(res / 60) % 60;
+  return mins;
+}
+
+function Trip(mins, miles, mph) {
+  this.mins = 0;
+  this.miles = 0;
+  this.mph = 0;
+}
+
+module.exports = {
+  timeDriving,
+  writeToFile,
+  speed,
+};
---
+++ app.test.js
@@ -0,0 +1,22 @@
+var functions = require("./app.js");
+describe("detect drivers and recrod history", () => {
+  test("mentions drivers in the object", () => {
+    let state = {
+      Dan: { mins: 50, miles: 39.1, mph: 47 },
+      Lauren: { mins: 15, miles: 42, mph: 168 },
+      Kumi: { mins: 0, miles: 0, mph: 0 },
+    };
+    expect(functions.writeToFile(state)).toMatch(/Kumi/);
+    expect(functions.writeToFile(state)).toMatch(/Lauren/);
+    expect(functions.writeToFile(state)).toMatch(/Dan/);
+  });
+
+  test("should accept strings and calcuate the speed", () => {
+    expect(functions.speed("17.3", "30")).toBeGreaterThan(0);
+    expect(functions.speed("17.3", "30")).toBeTruthy();
+  });
+
+  test("should calculate time driving", () => {
+    expect(functions.timeDriving("07:15", "07:45")).toBe(30);
+  });
+});
